12300de137c240e3c70a3911fabb4c3a23bf6f28
BobuDragos <46262576+BobuDragos@users.noreply.github.com>
2022-10-20 05:59:17 +0300

    Update lab2.py

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -202,6 +202,47 @@ def isPalindrome(input):
 """
 
 
+"""
+9. Write a function that receives as paramer 
+a matrix which represents the heights of the spectators 
+in a stadium and will return a list of tuples (line, column) 
+each one representing a seat of a spectator which can't see the game. 
+A spectator can't see the game if there is at least one taller spectator 
+standing in front of him. All the seats are occupied. All the seats are at the same level.
+ Row and column indexing starts from 0, beginning with the closest row from the field.
+"""
+
+"""
+10. Write a function that receives a variable number of lists 
+and returns a list of tuples as follows: the first tuple contains 
+the first items in the lists, the second element contains the items 
+on the position 2 in the lists, etc. 
+
+Ex: for lists [1,2,3], [5,6,7], ["a", "b", "c"] 
+return: [(1, 5, "a ") ,(2, 6, "b"), (3,7, "c")]. 
+Note: If input lists do not have the same number of items,
+missing items will be replaced with None to be able to generate 
+max ([len(x) for x in input_lists]) tuples.
+"""
+
+"""
+11. Write a function that will order a list of string 
+tuples based on the 3rd character of the 2nd element in the tuple.
+
+ Example: ('abc', 'bcd'), ('abc', 'zza')] ==> [('abc', 'zza'), ('abc', 'bcd')]
+"""
+
+"""
+12. Write a function that will receive a list of words  as parameter 
+and will return a list of lists of words, 
+grouped by rhyme. 
+Two words rhyme if both of them end with the same 2 letters.
+
+Example:
+group_by_rhyme(['ana', 'banana', 'carte', 'arme', 'parte']) 
+will return [['ana', 'banana'], ['carte', 'parte'], ['arme']] 
+"""
+
 if __name__ == '__main__':
     
     # pb1()

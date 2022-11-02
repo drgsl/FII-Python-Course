3ba06f4fdc9699634b320496e3e0438153fc7f85
Dragos A. Bobu <46262576+BobuDragos@users.noreply.github.com>
2022-11-03 01:23:23 +0200

    pycharm
    should be added to gitignore but idk how to do that without breaking everything

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -115,21 +115,21 @@ deci nu va apărea în lista finală.
 
 def pb4():
 
-dictionar_extensii = dict([])
+    dictionar_extensii = dict([])
 
-for file in os.listdir( sys.argv[1] ) :
-    
-    if os.path.isfile(file) :
-        for poz in range( len(file) - 1, 0, -1) :
-            
-            
-            if (file[poz] == '.') :
-                if (file[poz:] in dictionar_extensii) == False :
-                    dictionar_extensii[file[poz:]] = 1
-                else :
-                    dictionar_extensii[file[poz:]]  += 1
-                break
-print(dictionar_extensii)
+    for file in os.listdir( sys.argv[1] ) :
+        
+        if os.path.isfile(file) :
+            for poz in range( len(file) - 1, 0, -1) :
+
+                
+                if (file[poz] == '.') :
+                    if (file[poz:] in dictionar_extensii) == False :
+                        dictionar_extensii[file[poz:]] = 1
+                    else :
+                        dictionar_extensii[file[poz:]]  += 1
+                    break
+    print(dictionar_extensii)
 
 
 
